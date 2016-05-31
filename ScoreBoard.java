70bb7d7a6051742d054b2586fd68044e12920521
matti0cre <msaun99@gmail.com>
2016-05-31 12:12:29 -0400

    Update ScoreBoard.java

--- ScoreBoard.java
+++ ScoreBoard.java
@@ -1,20 +1,26 @@
-import java.awt.*; 
+import java.awt.*;  
 import javax.swing.*;
 public class ScoreBoard extends JPanel {
-	private int wins;
-	private int losses;
+	private int redWins;
+	private int redLosses;
+	private int blackWins;
+	private int blackLosses;
 	private JPanel board;
 	private JFrame frame;
 	private JLabel score;
-	public ScoreBoard(int w, int l) {
-		wins=w;
-		losses=l;
+	public ScoreBoard(int rw, int rl,int bw,int bl) {
+		redWins=rw;
+		redLosses=rl;
+		blackWins=bw;
+		blackLosses=bl;
 		board= new JPanel();		
 		frame= new JFrame("Score Board");
-		frame.setSize(250,75);
+		frame.setSize(750,75);
 		score= new JLabel();
 		score.setFont(new Font("Comic Sans MS",0,24));
-		score.setText("Wins: "+wins+"     Losses: "+losses);
+		score.setText("Red Wins: "+redWins+"     Red Losses: "
+		+redLosses+"   Black Wins: "+blackWins
+		+"    Black Losses: "+blackLosses);
 		score.setVisible(true);
 		board.add(score); 
 		board.setVisible(true);
